fd683f76390c64c4afdc6333a46a3e30a0188b4b
Toru MITANI <mitani@daisuke2.com>
2025-06-13 23:25:24 +0900

    メッセージが重複する不具合を修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,52 +1,48 @@
+
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # DeviseのRESTfulアクションを明示的に定義
-
   # GET /users/sign_up
   def new
+    if session[:registration_success_message]
+      flash.now[:notice] = session[:registration_success_message]
+      session.delete(:registration_success_message)
+    end
     super
   end
 
   # POST /users
   def create
     super do |resource|
-      if resource.persisted?
-        # デフォルトのフラッシュメッセージをクリア
-        flash.delete(:notice)
-        # カスタムメッセージを設定
-        flash[:notice] = "仮登録が完了しました。確認メールをお送りしましたので、メール内のリンクをクリックして本登録を完了してください。"
+      if resource.persisted? && !resource.active_for_authentication?
+        # Deviseのデフォルトメッセージはそのまま使用
+        # flash.clear を削除
+        # セッションにメッセージを保存して、リダイレクト後に表示
+        session[:registration_success_message] = flash[:notice]
+        flash.clear # リダイレクト前にクリア
       end
     end
-  end
-
-  # GET /users/edit
-  def edit
-    super
-  end
-
-  # PATCH/PUT /users
-  def update
-    super
-  end
-
-  # DELETE /users
-  def destroy
-    super
   end
 
   protected
 
   def after_sign_up_path_for(resource)
-    Rails.logger.debug "after_sign_up_path_for called for #{resource.email}"
-    # ユーザーをサインアウトさせて新規登録画面に遷移
     sign_out(resource)
     new_user_registration_path
   end
 
   def after_inactive_sign_up_path_for(resource)
-    Rails.logger.debug "after_inactive_sign_up_path_for called for #{resource.email}"
-    # 確認待ち状態でも新規登録画面にリダイレクト
     new_user_registration_path
   end
+
+  # これらのメソッドは不要なので削除
+  # def set_flash_message(key, kind, options = {})
+  #   return if kind == :signed_up_but_unconfirmed
+  #   super
+  # end
+
+  # def set_flash_message!(key, kind, options = {})
+  #   return if kind == :signed_up_but_unconfirmed
+  #   super
+  # end
 end
