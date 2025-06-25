cb601e6140b82c8de5880a0bb4c11b6ac4830f44
Toru MITANI <mitani@daisuke2.com>
2025-06-25 20:43:31 +0900

    github actionでエラーになっていたため修正

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,14 +1,23 @@
-
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 
-  # システムテスト用のヘルパーメソッド
-  def login_as(user)
+  # ヘッドレスモードでChromeを実行（CI環境対応）
+  if ENV["CI"]
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  end
+
+  # ログインヘルパーメソッド
+  def login_as(user, password: "Secure#P@ssw0rd9")
     visit new_user_session_path
     fill_in "user[email]", with: user.email
-    fill_in "user[password]", with: "Secure#P@ssw0rd9"
+    fill_in "user[password]", with: password
     click_button "ログイン"
+  end
+
+  # ログアウトヘルパーメソッド
+  def logout
+    click_link "ログアウト" if page.has_link?("ログアウト")
   end
 end
