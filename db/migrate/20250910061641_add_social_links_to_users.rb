b6d49faab5ce57261f5cdec716867f351e8a4f81
Toru MITANI <mitani@daisuke2.com>
2025-09-10 16:57:31 +0900

    投稿詳細画面に同じ投稿者の前・次投稿へのナビゲーション機能を追加

---
+++ db/migrate/20250910061641_add_social_links_to_users.rb
@@ -0,0 +1,8 @@
+class AddSocialLinksToUsers < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :twitter_url, :string
+    add_column :users, :github_url, :string
+    add_column :users, :linkedin_url, :string
+    add_column :users, :website_url, :string
+  end
+end
