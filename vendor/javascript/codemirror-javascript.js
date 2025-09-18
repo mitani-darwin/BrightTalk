098f6d3d96953e81742a4c9851ce6d40846d9a14
Toru MITANI <mitani@daisuke2.com>
2025-09-18 23:45:53 +0900

    JSのCloudFrontから取得対応

--- vendor/javascript/codemirror-javascript.js
+++ vendor/javascript/codemirror-javascript.js
@@ -282,7 +282,6 @@ CodeMirror.defineMode("javascript", function(config, parserConfig) {
     if (!trackScope) return
     if (state.context) {
       if (state.lexical.info == "var" && state.context && state.context.block) {
-        // FIXME function decls are also not block scoped
         var newContext = registerVarScoped(varname, state.context)
         if (newContext != null) {
           state.context = newContext
