08cee5016e2cc5ec91ba0d1d94c57ee559bf2f54
Toru MITANI <mitani@daisuke2.com>
2025-09-19 21:16:51 +0900

    DirectUploadが機能するように修正。

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_mailbox/engine"
 require "action_view/railtie"
-# require "action_cable/engine"  # Disabled - not used
-# require "action_text/engine"   # Disabled - not used
+require "action_cable/engine"  # Disabled - not used
+require "action_text/engine"   # Disabled - not used
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
