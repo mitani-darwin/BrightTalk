d5ccce0f9e4d43218756d6b928fea6276d594d29
Toru MITANI <mitani@daisuke2.com>
2025-10-18 12:49:52 +0900

    アイコンのファイルサイズの上限設定を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :avatar, content_type: { in: %w[image/jpeg image/png image/gif],
-                                     message: "JPEG、JPG、PNG、GIF形式のファイルを選択してください" },
-            size: { less_than: 5.megabytes, message: "5MB以下のファイルを選択してください" }
+                                     message: "JPEG、JPG、PNG、GIF形式のファイルを選択してください" }
   validates :header_image, content_type: { in: %w[image/jpeg image/png image/gif],
                                           message: "JPEG、JPG、PNG、GIF形式のファイルを選択してください" }
 
